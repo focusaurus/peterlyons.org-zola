92f43bec4cd24d2c7175d5b8f90e3c27dc341e12
Peter Lyons <pete@peterlyons.com>
2022-06-12 12:01:21 -0400

    hmm maybe zola 0.15.3 is fine

--- scripts-container/build.sh
+++ scripts-container/build.sh
@@ -15,7 +15,7 @@ IFS="$(printf "\n\t")"
 
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 export PATH="${PWD}/local/bin:${PATH}"
-ZOLA_VERSION=0.15.2
+ZOLA_VERSION=0.15.3
 zola_url="https://github.com/getzola/zola/releases/download/v${ZOLA_VERSION}/zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
 version=$(zola --version 2>/dev/null || /bin/true)
 if [[ ! "${version}" =~ ^zola ]]; then
