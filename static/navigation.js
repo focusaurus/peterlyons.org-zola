faf8cd9c5813ece772c9201242c9ca44972ead27
Peter Lyons <pete@peterlyons.com>
2024-09-01 11:49:24 -0400

    update to latest navigation code
    Handles /persblog/ path prefix correctly for finding current nav link

--- static/navigation.js
+++ static/navigation.js
@@ -7,7 +7,12 @@ function toggleNav() {
   content.classList.toggle("navOpen");
 }
 
-function closeNav() {
+function closeNav(event) {
+  if (event.target.classList.contains("navMenuButton")) {
+    // this is the click of the nav menu button to open it.
+    // Disregard.
+    return;
+  }
   nav.classList.remove("open");
   content.classList.remove("navOpen");
 }
@@ -15,10 +20,16 @@ function closeNav() {
 export default function start() {
   document.querySelector(".navMenuButton").addEventListener("click", toggleNav);
   document.querySelector("nav.site .close").addEventListener("click", closeNav);
+  document.addEventListener("click", closeNav);
 
-  const selector = `nav a[href='${document.location.pathname}']`;
+  let path = document.location.pathname;
+  const persblog = "/persblog/";
+  if (path.startsWith(persblog)) {
+    path = path.substring(0, persblog.length);
+  }
+  const selector = `nav a[href='${path}']`;
   const navEls = document.querySelectorAll(selector);
-  Array.prototype.forEach.call(navEls, el => {
+  Array.prototype.forEach.call(navEls, (el) => {
     el.classList.add("current");
   });
 }
