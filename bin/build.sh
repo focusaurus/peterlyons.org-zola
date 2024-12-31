feac0c67fc3a1e863a6e4ca9f218b3ab54afbb16
Peter Lyons <pete@peterlyons.com>
2024-12-31 10:20:49 -0500

    build with mise

--- bin/build.sh
+++ bin/build.sh
@@ -4,25 +4,17 @@
 
 # ---- Start unofficial bash strict mode boilerplate
 # http://redsymbol.net/articles/unofficial-bash-strict-mode/
-set -o errexit  # always exit on error
+set -o errexit # always exit on error
 set -o errtrace # trap errors in functions as well
 set -o pipefail # don't ignore exit codes when piping output
-set -o posix    # more strict failures in subshells
-# set -x          # enable debugging
+set -u # error on reference to unknown variable
+# set -x # enable debugging
 
-IFS="$(printf "\n\t")"
+IFS=$'\n\t'
 # ---- End unofficial bash strict mode boilerplate
 
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
-export PATH="${PWD}/local/bin:${PATH}"
-ZOLA_VERSION=0.15.3
-zola_url="https://github.com/getzola/zola/releases/download/v${ZOLA_VERSION}/zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
-version=$(zola --version 2>/dev/null || /bin/true)
-if [[ ! "${version}" =~ ^zola ]]; then
-  # install zola
-  mkdir -p ./local/bin
-  wget -q -O - "${zola_url}" |
-    tar --extract --gzip --file - --directory local/bin
-  chmod 755 local/bin/zola
+if  ! command -v mise >/dev/null; then
+  curl https://mise.run | sh
 fi
-zola build
+mise run build
