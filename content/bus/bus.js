d94adcaf0d6e5006dd62aa865e6d3d29a26e2b46
Peter Lyons <pete@peterlyons.com>
2025-02-07 07:07:08 -0500

    fix bus filters

--- content/bus/bus.js
+++ content/bus/bus.js
@@ -1,14 +1,35 @@
-const qsa = document.querySelectorAll.bind(document);
+const filters = {
+  direction: "east",
+  day: "weekday",
+};
 const remove = (selector, klass) => {
   document.querySelectorAll(selector).forEach((n) => n.classList.remove(klass));
 };
 const add = (selector, klass) => {
   document.querySelectorAll(selector).forEach((n) => n.classList.add(klass));
 };
-const toggle = (category, toShow) => {
-  remove(`.filter${category}`, "active");
-  add(`.filter${toShow}`, "active");
+
+const filterDirection = (klass) => {
+  filters.direction = klass;
+  display();
+};
+const filterDay = (klass) => {
+  filters.day = klass;
+  display();
+};
+
+const display = () => {
+  remove(`.filter`, "active");
+  add(`.filter.${filters.day}`, "active");
+  add(`.filter.${filters.direction}`, "active");
 
   add(".route", "hide");
-  remove(`.route${toShow}`, "hide");
+  document.querySelectorAll(".route").forEach((n) => {
+    if (
+      n.classList.contains(filters.direction) &&
+      n.classList.contains(filters.day)
+    ) {
+      n.classList.remove("hide");
+    }
+  });
 };
